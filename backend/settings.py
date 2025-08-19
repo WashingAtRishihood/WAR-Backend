3c9883fbf2a29bb31164eb64e014b3e847cb6441
Ritesh Kumar <7976ritesh@gmail.com>
2025-08-20 00:17:03 +0530

    made the changes for the updation of count

--- backend/settings.py
+++ backend/settings.py
@@ -156,6 +156,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default port
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://war-frontend.vercel.app/",  # Vercel deployment
 ]
 
 CORS_ALLOW_CREDENTIALS = True
