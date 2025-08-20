89abfb0e84ad4b356400a0ea6fb4ed51e877c281
Ritesh Kumar <7976ritesh@gmail.com>
2025-08-20 14:08:49 +0530

    added password

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     enrollment_no = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=128)  # Will be hashed
+    password = models.CharField(max_length=128, null=True, blank=True)  # Made optional initially
     phone_no = models.CharField(max_length=15)
     bag_no = models.CharField(
         max_length=20,
--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
-python manage.py migrate
+# Generate migrations
+python manage.py makemigrations --no-input
+
+# Apply migrations
+python manage.py migrate --no-input
 
 python manage.py ensure_superuser
